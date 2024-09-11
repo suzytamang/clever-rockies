bd8234bb47f632780d1fc99503fc4e73c2ab0166
Dax <11837526+dax-westerman@users.noreply.github.com>
2024-09-11 15:51:53 -0500

    Updated to streamline script to call from command line

--- src/step2/step2.sh
+++ src/step2/step2.sh
@@ -9,12 +9,74 @@ LGCONTEXT=3
 RGCONTEXT=2
 WORKERS=8
 
+main_targets=(XYLA A2AG PDMP CAFFINEDO BTRAITS BDD GAMINGDO GAMBLINGDO BEHAVIORAD MISOPHONIA IDU LONELINESS LIVESALONE JOBINSTABLE STRAUMA JUSTICE SOCIALCONNECT HOUSING DETOX LETHALMEANS FOODINSECURE ADL DODOUD IDU)
+
 cd "$(dirname "$0")"
 
-# below weshow an example bash command for Step 2, tagging of text.  Each example corresponds with a different target clinical practice.
+corpus_src="$1"
+output="$2"
+lexicon="$(realpath "$LEXICON")"
+headers="$(realpath "$HEADERS")"
 
-# xylazine
-python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets XYLA --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/XYLA --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
+test_notes=0
+if [[ "$corpus_src"=="test_notes" ]]; then
+    echo >&2 "Using test notes"
+    test_notes=1
+    corpus_src="$(realpath "../../tests/resources/test_notes/test_notes_with_metadata_one_line.txt")"
+fi
+
+# below we show an example bash command for Step 2, tagging of text.  Each example corresponds with a different target clinical practice.
+
+run_sequencer() {
+
+    local main_target="$1"
+    local corpus_src="$2"
+    local output_path="$3"
+    local lexicon="$4"
+    local headers="$5"
+
+    local full_output_path="$(realpath "${output_path}/${main_target}")"
+
+    echo >&2 "output_path: $output_path"
+    echo >&2 "corpus_src: $corpus_src"
+    echo >&2 "lexicon: $lexicon"
+    echo >&2 "headers: $headers"
+
+    if [[ -z "$main_target" ]]; then
+        echo >&2 "Main target empty"
+        echo "1"
+    else
+        echo >&2 "Running Sequencer on Main Target: \"$main_target\""
+        cmd="python sequencer.py --lexicon \""${lexicon}"\" --section-headers \""${headers}"\" --main-targets \""${main_target}"\" --snippet-length \""${SNIPPETS}"\" --snippets --notes \""${corpus_src}"\" --workers \""${WORKERS}"\" --output \""${full_output_path}"\" --left-gram-context \""${LGCONTEXT}"\" --right-gram-context \""${RGCONTEXT}"\""
+        echo >&2 "$cmd"
+        echo "0"
+    fi
+    
+}
+
+if [[ -z "$corpus_src" ]]; then
+    corpus_src="$CORPUS"
+fi
+
+if [[ -z "$output" ]]; then
+    output="$OUTPUT"
+fi
+
+echo "Processing corpus: $corpus_src"
+echo "Output Root: $output"
+echo "Lexicon: $lexicon"
+echo "Headers: $headers"
+
+for m in ${main_targets[@]}; do
+    results="$(run_sequencer "$m" "$corpus_src" "$output" "$lexicon" "$headers")"
+    if [[ "$results" == "1" ]]; then
+        echo >&2 "Error encountered; exiting at step \"$m\""
+        exit "1"
+    fi
+done
+
+# # xylazine
+# python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets XYLA --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/XYLA --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
 # # alpha 2 agonists
 # python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets A2AG --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/A2AG --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
@@ -22,7 +84,7 @@ python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets
 # # prescription drug monitoring program
 # python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets PDMP --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/PDMP --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
-# # caffeine use disorder                                                                                                         
+# # caffeine use disorder
 # python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets CAFFINEDO --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/CAFFINEDO --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
 # # cluster b traits
@@ -40,7 +102,7 @@ python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets
 # # other behavioral addiction
 # python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets BEHAVIORAD --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/BEHAVIORAD --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
-# #misophonia                                                                                                                                  
+# #misophonia
 # python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets MISOPHONIA --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/MISOPHONIA --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
 # # injection drug use
@@ -79,8 +141,7 @@ python sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets
 # # acitivites of daily living.  Specifically, bathing, dressing and eating.
 # python3 sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets ADL --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/ADL --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
-# department of defense opioid use disorder
-python3 sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets DODOUD --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/DODOUD --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
+# # department of defense opioid use disorder
+# python3 sequencer.py --lexicon $LEXICON --section-headers $HEADERS --main-targets DODOUD --snippet-length $SNIPPETS --snippets --notes $CORPUS --workers $WORKERS --output $OUTPUT/DODOUD --left-gram-context $LGCONTEXT --right-gram-context $RGCONTEXT
 
 #python3 sequencer.py --lexicon ../../res/dicts/dict.txt --section-headers ../../res/headers.txt --main-targets IDU --snippet-length 200 --snippets --notes /tmp/workspacezone/tsv/lastmodifieddatetime_month=202204.txt --workers 8 --output ../../output/IDU --left-gram-context 3 --right-gram-context 2
-
