9783b00739b9fe8c2255b8da6fc41d219e5ac5f4
Dax <11837526+dax-westerman@users.noreply.github.com>
2024-09-12 07:58:10 -0500

    Using explicit imports

--- src/step3/organize.py
+++ src/step3/organize.py
@@ -1,14 +1,16 @@
 import os, sys, operator
-from step3fcn import *
 
-ppath = sys.argv[1]      #project path where the output of step2 lives
-dictfile = sys.argv[2]   #terminology file
-noteMdata = sys.argv[3]  #note metadata
-ptAntfile = sys.argv[4]  #annotation file
+from step3.step3fcn import getTerminology, loadSeqs, loadVANoteMdata
+
+
+ppath = sys.argv[1]  # project path where the output of step2 lives
+dictfile = sys.argv[2]  # terminology file
+noteMdata = sys.argv[3]  # note metadata
+ptAntfile = sys.argv[4]  # annotation file
 
 # TARGET CLASSES #
 
-termDict = getTerminology(dictfile)  #get terminology
+termDict = getTerminology(dictfile)  # get terminology
 
 noteMDict = loadVANoteMdata(noteMdata)
 print("NOTES:", len(noteMDict))
@@ -16,15 +18,15 @@ print("NOTES:", len(noteMDict))
 seqDict = {}
 
 seqFile = ppath + "/extraction*.tsv"
-    #print seqFile
-    # passing loadSeqs the extraction files, note metadata and dictionary
-tmpDict = loadSeqs(seqFile,noteMDict,termDict)
-    #print tmpDict
+# print seqFile
+# passing loadSeqs the extraction files, note metadata and dictionary
+tmpDict = loadSeqs(seqFile, noteMDict, termDict)
+# print tmpDict
 seqDict.update(tmpDict)
 print("Length of Sequence Dictionary:", len(seqDict))
 print("CANDIDATE EVENTS:", str(len(seqDict)))
 
-fout = open(ptAntfile,"w")
+fout = open(ptAntfile, "w")
 for sid in seqDict:
     tmp = seqDict[sid]
     sinfo = tmp.split("|")
