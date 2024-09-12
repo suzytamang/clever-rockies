bef85400bf600c8445a9f9ab13874be48b8a36cb
Dax <11837526+dax-westerman@users.noreply.github.com>
2024-09-12 13:40:47 -0500

    Updated to permit external env var

--- src/step3/step3.sh
+++ src/step3/step3.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/bash
 
-LEXICON=../../res/dicts/dict.txt
-METADATA=/tmp/workspacezone/metadata/lastmodifieddatetime_month=202210.txt
-OUTPUT=../../output202210
-ANTS=linkedAnts.txt
 
 cd "$(dirname "$0")"
 
-python3 organize.py $OUTPUT/XYLA $LEXICON $METADATA $OUTPUT/XYLA/$ANTS
-python3 organize.py $OUTPUT/A2AG $LEXICON $METADATA $OUTPUT/A2AG/$ANTS
-python3 organize.py $OUTPUT/PDMP $LEXICON $METADATA $OUTPUT/PDMP/$ANTS
-python3 organize.py $OUTPUT/CAFFINEDO $LEXICON $METADATA $OUTPUT/CAFFINEDO/$ANTS
-python3 organize.py $OUTPUT/BTRAITS $LEXICON $METADATA $OUTPUT/BTRAITS/$ANTS
-python3 organize.py $OUTPUT/BDD $LEXICON $METADATA $OUTPUT/BDD/$ANTS
-python3 organize.py $OUTPUT/GAMINGDO $LEXICON $METADATA $OUTPUT/GAMINGDO/$ANTS
-python3 organize.py $OUTPUT/GAMBLINGDO $LEXICON $METADATA $OUTPUT/GAMBLINGDO/$ANTS
-python3 organize.py $OUTPUT/BEHAVIORAD $LEXICON $METADATA $OUTPUT/BEHAVIORAD/$ANTS
-python3 organize.py $OUTPUT/MISOPHONIA $LEXICON $METADATA $OUTPUT/MISOPHONIA/$ANTS
-python3 organize.py $OUTPUT/IDU $LEXICON $METADATA $OUTPUT/IDU/$ANTS
-python3 organize.py $OUTPUT/JOBINSTABLE $LEXICON $METADATA $OUTPUT/JOBINSTABLE/$ANTS
-python3 organize.py $OUTPUT/JUSTICE $LEXICON $METADATA $OUTPUT/JUSTICE/$ANTS
-python3 organize.py $OUTPUT/LIVESALONE $LEXICON $METADATA $OUTPUT/LIVESALONE/$ANTS
-python3 organize.py $OUTPUT/LONELINESS $LEXICON $METADATA $OUTPUT/LONELINESS/$ANTS
-python3 organize.py $OUTPUT/SOCIALCONNECT $LEXICON $METADATA $OUTPUT/SOCIALCONNECT/$ANTS
-python3 organize.py $OUTPUT/STRAUMA $LEXICON $METADATA $OUTPUT/STRAUMA/$ANTS
-python3 organize.py $OUTPUT/HOUSING $LEXICON $METADATA $OUTPUT/HOUSING/$ANTS
-python3 organize.py $OUTPUT/DETOX $LEXICON $METADATA $OUTPUT/DETOX/$ANTS
-python3 organize.py $OUTPUT/FOODINSECURE $LEXICON $METADATA $OUTPUT/FOODINSECURE/$ANTS
-python3 organize.py $OUTPUT/LETHALMEANS $LEXICON $METADATA $OUTPUT/LETHALMEANS/$ANTS
-python3 organize.py $OUTPUT/ADL $LEXICON $METADATA $OUTPUT/ADL/$ANTS
-python3 organize.py $OUTPUT/DODOUD $LEXICON $METADATA $OUTPUT/DODOUD/$ANTS
+python_path="${PYTHON_EXE:=python}"
+LEXICON="$(realpath ../../res/dicts/dict.txt)"
+ANTS=linkedAnts.txt
+if [[ ! -z "${METADATA_PATH}" ]]; then
+    METADATA="${METADATA_PATH}"
+else
+    METADATA="$(realpath /tmp/workspacezone/METADATA/lastmodifieddatetime_month=202210.txt)"
+fi
+if [[ ! -z "${CLEVER_OUTPUT}" ]]; then
+    OUTPUT="${CLEVER_OUTPUT}"
+else
+    OUTPUT="$(realpath ../../output202210)"
+fi
+
+"${python_path}" organize.py "${OUTPUT}/XYLA" "${LEXICON}" "${METADATA}" "${OUTPUT}/XYLA/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/A2AG" "${LEXICON}" "${METADATA}" "${OUTPUT}/A2AG/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/PDMP" "${LEXICON}" "${METADATA}" "${OUTPUT}/PDMP/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/CAFFINEDO" "${LEXICON}" "${METADATA}" "${OUTPUT}/CAFFINEDO/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/BTRAITS" "${LEXICON}" "${METADATA}" "${OUTPUT}/BTRAITS/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/BDD" "${LEXICON}" "${METADATA}" "${OUTPUT}/BDD/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/GAMINGDO" "${LEXICON}" "${METADATA}" "${OUTPUT}/GAMINGDO/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/GAMBLINGDO" "${LEXICON}" "${METADATA}" "${OUTPUT}/GAMBLINGDO/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/BEHAVIORAD" "${LEXICON}" "${METADATA}" "${OUTPUT}/BEHAVIORAD/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/MISOPHONIA" "${LEXICON}" "${METADATA}" "${OUTPUT}/MISOPHONIA/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/IDU" "${LEXICON}" "${METADATA}" "${OUTPUT}/IDU/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/JOBINSTABLE" "${LEXICON}" "${METADATA}" "${OUTPUT}/JOBINSTABLE/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/JUSTICE" "${LEXICON}" "${METADATA}" "${OUTPUT}/JUSTICE/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/LIVESALONE" "${LEXICON}" "${METADATA}" "${OUTPUT}/LIVESALONE/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/LONELINESS" "${LEXICON}" "${METADATA}" "${OUTPUT}/LONELINESS/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/SOCIALCONNECT" "${LEXICON}" "${METADATA}" "${OUTPUT}/SOCIALCONNECT/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/STRAUMA" "${LEXICON}" "${METADATA}" "${OUTPUT}/STRAUMA/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/HOUSING" "${LEXICON}" "${METADATA}" "${OUTPUT}/HOUSING/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/DETOX" "${LEXICON}" "${METADATA}" "${OUTPUT}/DETOX/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/FOODINSECURE" "${LEXICON}" "${METADATA}" "${OUTPUT}/FOODINSECURE/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/LETHALMEANS" "${LEXICON}" "${METADATA}" "${OUTPUT}/LETHALMEANS/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/ADL" "${LEXICON}" "${METADATA}" "${OUTPUT}/ADL/$ANTS"
+"${python_path}" organize.py "${OUTPUT}/DODOUD" "${LEXICON}" "${METADATA}" "${OUTPUT}/DODOUD/$ANTS"
