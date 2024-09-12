7e2643e9caa8a8ef5e85bf528db1fce397525735
Dax <11837526+dax-westerman@users.noreply.github.com>
2024-09-12 11:40:58 -0500

    Formatting on cleverRules.py

--- src/step4/cleverRules.py
+++ src/step4/cleverRules.py
@@ -1,4 +1,4 @@
-#TODO: Remove output to screen.
+# TODO: Remove output to screen.
 
 import os, sys, re
 from ruleFcns import *
@@ -11,22 +11,34 @@ print("Processing:", fins)
 ptPEvents = {}
 ptNEvents = {}
 ptEvents = {}
-fout_pos = open(ppath+"/allPos_unfiltered.txt","w")
-fout_neg = open(ppath+"/allNeg_unfiltered.txt","w")
-fout_na = open(ppath+"/allNA_unfiltered.txt","w")
-fout_all = open(ppath+"/labeledAnts.txt","w")
-trigs = ["NEGEX","HX","HYP","SCREEN","RISK","FAM","PREV"]
+fout_pos = open(ppath + "/allPos_unfiltered.txt", "w")
+fout_neg = open(ppath + "/allNeg_unfiltered.txt", "w")
+fout_na = open(ppath + "/allNA_unfiltered.txt", "w")
+fout_all = open(ppath + "/labeledAnts.txt", "w")
+trigs = ["NEGEX", "HX", "HYP", "SCREEN", "RISK", "FAM", "PREV"]
 
 
-print("label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",file=fout_pos)
-print("label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",file=fout_neg)
-print("label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",file=fout_na)
-print("label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",file=fout_all)
+print(
+    "label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",
+    file=fout_pos,
+)
+print(
+    "label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",
+    file=fout_neg,
+)
+print(
+    "label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",
+    file=fout_na,
+)
+print(
+    "label|snippetID|term|sta3n|TIUdocumentSID|TIUstandardTitle|visitSID|referenceDateTime|PatientSID|targetClass|targetSubClass|termID|NoteAndSnipOffset|snippet|OpCode",
+    file=fout_all,
+)
 
 with open(fins) as f:
     for line in f:
         tmp = line.strip()
-        #print("TEMP",tmp)
+        # print("TEMP",tmp)
         if aclass not in tmp:
             continue
         label = assignLabel(tmp, trigs)
@@ -48,42 +60,72 @@ with open(fins) as f:
         age = tmpe[16]
         gender = tmpe[17]
         upcode = tmpe[18]
-        snippet = tmpe[len(tmpe)-1]
+        snippet = tmpe[len(tmpe) - 1]
         # SNIPPET POSTPROCESSING
         # remove the first and last token in the snippet to help readability"
         tokens = snippet.split(" ")
         if len(tokens) >= 3:
-                tmp = snippet.rsplit(' ', 1)[0]
-                tmp = ' '.join(tmp.split()[2:])
-                snippet = "SNIPPET:"+tmp
+            tmp = snippet.rsplit(" ", 1)[0]
+            tmp = " ".join(tmp.split()[2:])
+            snippet = "SNIPPET:" + tmp
         # add the snippet offset to the output field "noteAndSnippetOffset"
         x = term_offsets(longseq, snippet)
-        termid = termid+":"+str(x)
-        #print(termid, longseq, snippet)
-        #print(termid,longseq,snippet)
-                
-        sum_out = label[0]+"|"+cid+"|"+longseq+"|"+tterm+"|"+pid+"|"+nid+"|"+ntype+"|"+time+"|"+year+"|"+tclass+"|"+tsclass+"|"+noffset+"|"+termid+"|"+snippet+"|"+upcode
+        termid = termid + ":" + str(x)
+        # print(termid, longseq, snippet)
+        # print(termid,longseq,snippet)
 
-        long_out =  label[0]+"|"+label[1]+"|"+tmp
+        sum_out = (
+            label[0]
+            + "|"
+            + cid
+            + "|"
+            + longseq
+            + "|"
+            + tterm
+            + "|"
+            + pid
+            + "|"
+            + nid
+            + "|"
+            + ntype
+            + "|"
+            + time
+            + "|"
+            + year
+            + "|"
+            + tclass
+            + "|"
+            + tsclass
+            + "|"
+            + noffset
+            + "|"
+            + termid
+            + "|"
+            + snippet
+            + "|"
+            + upcode
+        )
+
+        long_out = label[0] + "|" + label[1] + "|" + tmp
 
         tmpE = ""
         if label[0] == "POSITIVE":
-                print(sum_out, file=fout_pos)
-                print(long_out, file=fout_all)
+            print(sum_out, file=fout_pos)
+            print(long_out, file=fout_all)
         if label[0] == "NEGATIVE":
-                print(sum_out, file=fout_neg)
-                print(long_out, file=fout_all)
+            print(sum_out, file=fout_neg)
+            print(long_out, file=fout_all)
         if label[0] == "NO_APPLICABLE":
-                print(sum_out, file=fout_na)
-                print(long_out, file=fout_all)
+            print(sum_out, file=fout_na)
+            print(long_out, file=fout_all)
 fout_pos.close()
 fout_neg.close()
 print("WROTE POS NEG NA FILES")
 
-#print len(ptPEvents)
-#print len(ptNEvents)
-#print len(ptEvents)
-#for pid in ptPEvents:
+# print len(ptPEvents)
+# print len(ptNEvents)
+# print len(ptEvents)
+# for pid in ptPEvents:
 #        fpos = open(opath+"pt"+pid+"_pos.txt","w")
 #        te = ptPEvents[pid]
 #        for i in range(len(te)):
@@ -92,25 +134,25 @@ print("WROTE POS NEG NA FILES")
 #                print pid,tevent
 #        fpos.close()
 
-#for pid in ptNEvents:
+# for pid in ptNEvents:
 #        fneg = open(opath+"pt"+pid+"_neg.txt","w")
 #        te = ptNEvents[pid]
 #        for i in range(len(te)):
-#                tevent = te[i] 
+#                tevent = te[i]
 #                print >> fneg,tevent
 #                print pid,tevent
 #        fneg.close()
 
-#for pid in ptEvents:
+# for pid in ptEvents:
 #        fcron = open(opath+"pt"+pid+"_cronology.txt","w")
 #        te = ptEvents[pid]
 #        for i in range(len(te)):
-#                tevent = te[i] 
+#                tevent = te[i]
 #                print >> fcron,tevent
 #                print pid,tevent
 #        fcron.close()
 
-#s =assignMBCLabel(snippet)
-#print s
+# s =assignMBCLabel(snippet)
+# print s
 
-#s = assignMBCLabel(s2)
+# s = assignMBCLabel(s2)
