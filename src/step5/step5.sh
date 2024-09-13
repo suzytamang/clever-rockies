f44888de96361c7171f5a5ff3d9cb589e0f1659e
Dax <11837526+dax-westerman@users.noreply.github.com>
2024-09-13 06:27:29 -0500

    processing step 5

--- src/step5/step5.sh
+++ src/step5/step5.sh
@@ -1,38 +1,47 @@
 #!/usr/bin/bash
 
+python_path="${PYTHON_EXE:=python}"
+script_directory="$(realpath "$0" | sed 's|\(.*\)/.*|\1|')"
+common_directory="$(dirname "${script_directory}")/common"
+project_directory="$(dirname "${script_directory}")"
+res_directory="$(dirname "${project_directory}")/res"
+
+# echo "python_path: ${python_path}"
+# echo "script_directory: ${script_directory}"
+# echo "project_directory: ${project_directory}"
+# echo "common_directory: ${common_directory}"
+# echo "res_directory: ${res_directory}"
+
 #LEXICON=$1
-OUTPUT=../../output202210
 #CROSSCLASS=$2
-ASSESSMENTTERMS=../../res/assessment_terms.txt
-OTHERTERMS=../../res/other_terms_to_drop.txt
+if [[ ! -z "${CLEVER_OUTPUT}" ]]; then
+    OUTPUT="${CLEVER_OUTPUT}"
+else
+    OUTPUT="$(realpath ../../output202210)"
+fi
+echo >&2 "OUTPUT: ${OUTPUT}"
 
-terms=(
-  XYLA
-  A2AG
-  PDMP 
-  CAFFINEDO 
-  BTRAITS 
-  BDD 
-  GAMINGDO 
-  GAMBLINGDO 
-  BEHAVIORAD 
-  MISOPHONIA 
-  IDU 
-  JOBINSTABLE 
-  JUSTICE 
-  LIVESALONE 
-  LONELINESS 
-  SOCIALCONNECT 
-  STRAUMA 
-  HOUSING 
-  DETOX 
-  LETHALMEANS 
-  FOODINSECURE 
-  ADL
-  DODOUD
-)
+if [[ ! -z "${CLEVER_ASSESSMENTTERMS}" ]]; then
+    ASSESSMENTTERMS="${CLEVER_ASSESSMENTTERMS}"
+else
+    ASSESSMENTTERMS="$(realpath "${res_directory}/assessment_terms.txt")"
+fi
+echo >&2 "ASSESSMENTTERMS: ${ASSESSMENTTERMS}"
 
-cd "$(dirname "$0")"
+if [[ ! -z "${CLEVER_OTHERTERMS}" ]]; then
+    OTHERTERMS="${CLEVER_OTHERTERMS}"
+else
+    OTHERTERMS="$(realpath "${res_directory}"/other_terms_to_drop.txt)"
+fi
+echo >&2 "OTHERTERMS: ${OTHERTERMS}"
+
+main_targets_path="${common_directory}/main_targets"
+
+oldIFS="$IFS"
+IFS=$'\n' terms=($(<"$main_targets_path"))
+IFS="$oldIFS"
+
+pushd "$script_directory" > /dev/null
 
 for TARGET in "${terms[@]}"
 do
@@ -40,5 +49,7 @@ do
   #python3 cross_tagging_removal.py -d $LEXICON -t $OUTPUT/$TARGET -c $CROSSCLASS
 
   echo "Filtering tagged templated text for $TARGET"
-  python3 filterTemplated.py -p $OUTPUT/ -t $TARGET -a $ASSESSMENTTERMS -o $OTHERTERMS -sp 14 -tp 6
+  "${python_path}" filterTemplated.py -p "${OUTPUT}"/ -t "${TARGET}" -a "${ASSESSMENTTERMS}" -o "${OTHERTERMS}" -sp 14 -tp 6
 done
+
+popd > /dev/null
